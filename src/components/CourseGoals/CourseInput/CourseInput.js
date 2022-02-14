073f73625e73eecee3f2e133e6f20a760dd84fe8
Tabby Douglass <tabbytoney@gmail.com>
2022-02-14 16:36:09 -0600

    Conditional styling within the component with styled-components package

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,6 +2,44 @@ import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+import styled from 'styled-components';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${(props) => (props.invalid ? 'red' : 'black')}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+  }
+}
+`;
+
+// Special styles, deleted after adding conditionals to & input border/background
+// &.invalid input {
+//   border-color: red;
+//   background: #ffd7d7;
+//   }
+
+// &.invalid label {
+//   color: red;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -28,13 +66,18 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
+      <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type='text' onChange={goalInputChangeHandler} />
+      </FormControl>
+      {/* Used before stying added to component in FormControl
       <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input
-          type='text'
-          onChange={goalInputChangeHandler}
-        />
-      </div>
+        <input type='text' onChange={goalInputChangeHandler} />
+      </div> 
+      
+      Can also do it this way:
+      FormControl className={!isValid && 'invalid'}*/}
       <Button type='submit'>Add Goal</Button>
     </form>
   );
